1ee203e5416c461958573c3a87b2165666c5d038
shivam3692 <78149478+shivam3692@users.noreply.github.com>
2022-11-16 11:42:20 +0530

    f commit

---
+++ dTob/src/com/project/binary2Decimal.java
@@ -0,0 +1,12 @@
+package com.project;
+import java.util.*;
+public class binary2Decimal {
+    public static void b2d(int n){
+
+    }
+    public static void main(String[] args){
+
+        Scanner sc =new Scanner(System.in);
+        int  n =sc.nextInt();
+    }
+}
